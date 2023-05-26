27b48a0f95b4784b4007fc6ec757674bffc2fdb9
Mbiel <cain48108@gmail.com>
2023-05-26 19:16:08 -0300

    tela de logIn adicionada

--- A3/src/App.java
+++ A3/src/App.java
@@ -1,5 +1,33 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("views/login"));
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    public static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
---
+++ A3/src/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button btLogin;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
